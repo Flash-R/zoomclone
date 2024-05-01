3b058186d39339b18e2e50bcc046530c04ccf6da
Tumwesigye Ronnie <tumronald1@gmail.com>
2024-05-01 09:32:52 +0300

    Initialisation of project and working on sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { sidebarLinks } from '@/constants/SidebarLinks';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+function Sidebar() {
+    const pathName = usePathname();
+  return (
+    <section className='sticky top-0 left-0 w-fit h-screen flex-col bg-dark-1
+     justify-between p-6 pt-28 text-white max-sm:hidden lg:w-[264px]' style={{backgroundColor: "#1C1F2E"}}>
+        <div className='flex flex-col flex-1 gap-6'>
+            {sidebarLinks.map((link) => {
+                const isActive = pathName === link.route || pathName.startsWith(link.route);
+
+                return (
+                    <Link 
+                        href={link.route} 
+                        key={link.label}
+                        className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',
+                        {'bg-blue-1': isActive,}
+                        )}
+                    >
+                        <Image 
+                            src={link.icon} 
+                            alt={link.label} 
+                            width={24} 
+                            height={24} 
+                        />
+                        <p className='text-lg font-semibold max-lg:hidden'>
+                            {link.label}
+                        </p>
+                    </Link>
+                )
+            })}
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
---
+++ constants/SidebarLinks.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    icon: "/icons/home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    icon: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    icon: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    icon: "/icons/Video.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal",
+    icon: "/icons/add-personal.svg",
+    route: "/personal",
+  }
+];
